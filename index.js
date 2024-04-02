3aada7d1fabd7a547b8968bd774089c662173f85
AbdugafforovMurodjon <ume8156@gamil.com>
2024-04-02 23:45:42 +0500

    vazifa_5

---
+++ index.js
@@ -0,0 +1,11 @@
+function ikkiSonKopaytir(son1, son2) {
+    return son1 * son2;
+}
+
+let son1 = parseFloat(prompt("Birinchi sonni kiriting:"));
+let son2 = parseFloat(prompt("Ikkinchi sonni kiriting:"));
+
+let natija = ikkiSonKopaytir(son1, son2);
+alert("ikkiSonKopaytir: " + natija);
+
+
